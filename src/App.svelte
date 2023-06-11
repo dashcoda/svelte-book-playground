8aff8c3818594a444dd5767a0ce230d1499c037f
seanharris <sh0704@outlook.com>
2023-06-12 02:28:29 +0900

    initial comit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+import Button from "./Button.svelte";
+
+const label = 'ボタン'
+const user1 = {
+  firstName: 'sean',
+  id: '2378467',
+  bio: 'this is bio'
+}
+</script>
+
+<Button disabled {label} user = {user1}/>
+
+<style>
+
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let label = ''
+    export let user;
+    export let disabled = false
+</script>
+
+<button {disabled}>{label}</button>
+<h2>{user.firstName}(id: {user.id})</h2>
+<p>{user.bio}</p>
+
+<style></style>
